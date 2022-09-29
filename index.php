cd2683db1c426c86f71a46a77c741897f2c40d2d
crigaux <crigaux057@gmail.com>
2022-09-29 10:06:52 +0200

    htaccess

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+
+if(isset($_COOKIE['displayMode']) && isset($_COOKIE['nbPages']) && isset($_COOKIE['subjects'])){
+    header('Location: /home');
+    exit();
+} else {
+    header('Location: /parameters');
+    exit();
+}
